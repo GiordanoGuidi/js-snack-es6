19b54783c647805fabcb712f803fe858fc104fc1
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-16 17:01:38 +0100

    added over70Grades array

--- JS/script.js
+++ JS/script.js
@@ -1,27 +1,75 @@
-const tableName = 'Tavolo Vip';
+//#SNACK 1
+// const tableName = 'Tavolo Vip';
 
-const guests = [
-  'Brad Pitt',
-  'Johnny Depp',
-  'Lady Gaga',
-  'Cristiano Ronaldo',
-  'Georgina Rodriguez',
-  'Chiara Ferragni',
-  'Fedez',
-  'George Clooney',
-  'Amal Clooney',
-  'Maneskin'
-];
-console.log(guests)
+// const guests = [
+//   'Brad Pitt',
+//   'Johnny Depp',
+//   'Lady Gaga',
+//   'Cristiano Ronaldo',
+//   'Georgina Rodriguez',
+//   'Chiara Ferragni',
+//   'Fedez',
+//   'George Clooney',
+//   'Amal Clooney',
+//   'Maneskin'
+// ];
+// console.log(guests)
 //CREO UN NUOVO ARRAI DI OGGETTI
-const guestList = guests.map((element,i)=>{
-    return {
-        name: element,
-        seat: i+1,
-        table: tableName
+// const guestList = guests.map((element,i)=>{
+//     return {
+//         name: element,
+//         seat: i+1,
+//         table: tableName
+//     }
+// })
+// console.log(guestList)
+
+//#SNACK 2
+const studentsList = [
+    {
+        id : 213,
+        name: 'Marco della Rovere',
+        grades: 78
+    },
+    {
+        id : 110,
+        name: 'Paola Cortellessa',
+        grades: 96
+    },
+    {
+        id : 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id : 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id : 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id : 102,
+        name: 'Piero della Francesca',
+        grades: 50
+    },
+    {
+        id : 120,
+        name: 'Francesca da Polenta',
+        grades: 84
     }
+]
+
+//CREO ARRAY STUDENTI CONTOTALE VOTI < 70
+
+const over70Grades = studentsList.filter(student =>{
+    if(student.grades > 70) return true;
+    else false;
 })
-console.log(guestList)
+console.log(over70Grades)
 
 
 
--- README.md
+++ README.md
@@ -24,7 +24,7 @@ SNACK 2
 Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
 Per preparare l'aula di un nuovo corso, dobbiamo svolgere una serie di operazioni
 1. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
-2.Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 3.  dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);
 Questo è l'elenco degli studenti:
 Id|Name|Grades
