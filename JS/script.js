e47e0c5178da86b3ffe46b5eee6d6daaf84d7baf
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-01-17 16:41:22 +0100

    added array of objects

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-//#SNACK 1
+// //#SNACK 1
 // const tableName = 'Tavolo Vip';
 
 // const guests = [
@@ -14,7 +14,7 @@
 //   'Maneskin'
 // ];
 // console.log(guests)
-//CREO UN NUOVO ARRAI DI OGGETTI
+// CREO UN NUOVO ARRAI DI OGGETTI
 // const guestList = guests.map((element,i)=>{
 //     return {
 //         name: element,
@@ -24,61 +24,92 @@
 // })
 // console.log(guestList)
 
-//#SNACK 2
-const studentsList = [
-    {
-        id : 213,
-        name: 'Marco della Rovere',
-        grades: 78
-    },
-    {
-        id : 110,
-        name: 'Paola Cortellessa',
-        grades: 96
-    },
-    {
-        id : 250,
-        name: 'Andrea Mantegna',
-        grades: 48
-    },
-    {
-        id : 145,
-        name: 'Gaia Borromini',
-        grades: 74
-    },
-    {
-        id : 196,
-        name: 'Luigi Grimaldello',
-        grades: 68
-    },
-    {
-        id : 102,
-        name: 'Piero della Francesca',
-        grades: 50
-    },
-    {
-        id : 120,
-        name: 'Francesca da Polenta',
-        grades: 84
-    }
-]
+// //#SNACK 2
+// const studentsList = [
+//     {
+//         id : 213,
+//         name: 'Marco della Rovere',
+//         grades: 78
+//     },
+//     {
+//         id : 110,
+//         name: 'Paola Cortellessa',
+//         grades: 96
+//     },
+//     {
+//         id : 250,
+//         name: 'Andrea Mantegna',
+//         grades: 48
+//     },
+//     {
+//         id : 145,
+//         name: 'Gaia Borromini',
+//         grades: 74
+//     },
+//     {
+//         id : 196,
+//         name: 'Luigi Grimaldello',
+//         grades: 68
+//     },
+//     {
+//         id : 102,
+//         name: 'Piero della Francesca',
+//         grades: 50
+//     },
+//     {
+//         id : 120,
+//         name: 'Francesca da Polenta',
+//         grades: 84
+//     }
+// ]
 
 //CREO ARRAY STUDENTI CONTOTALE VOTI < 70
 
-const over70Grades = studentsList.filter(student =>{
-    if(student.grades > 70) return true;
-    else false;
-})
-console.log('sono fuori', over70Grades)
+// const over70Grades = studentsList.filter(student =>{
+//     if(student.grades > 70) return true;
+//     else false;
+// })
+// console.log('sono fuori', over70Grades)
 // CREO ARRAY CON TOTALE VOTI > A 70 E ID > 120
-const over70GradesAndIdOver120 = over70Grades.filter(student=>{
-    if(student.id > 120) return true;
-    else return false;
-})
-console.log('OVER 70 E 120', over70GradesAndIdOver120)
+// const over70GradesAndIdOver120 = over70Grades.filter(student=>{
+//     if(student.id > 120) return true;
+//     else return false;
+// })
+// console.log('OVER 70 E 120', over70GradesAndIdOver120)
 //CREO ARRAY CON NOMI DEGLI STUDENTI IN MAIUSCOLO
-const studentsNames = studentsList.map(student=>student.name.toUpperCase())
-console.log(studentsNames)
+// const studentsNames = studentsList.map(student=>student.name.toUpperCase())
+// console.log(studentsNames)
+// console.log(studentsList)
+
+//#-------------------------------------------------------
+
+//#SNACK 3
+//CREO UN ARRAY DI OGGETTI
+const bikes = [
+    {
+        name: 'simona',
+        weigth: 10
+    },
+    {
+        name: 'marcella',
+        weigth: 20
+    },
+    {
+        name: 'jhonny',
+        weigth: 15
+    },
+    {
+        name: 'clara',
+        weigth: 12
+    },
+    {
+        name: 'sonia',
+        weigth: 17
+    },
+]
+
+
+
 
 
 
